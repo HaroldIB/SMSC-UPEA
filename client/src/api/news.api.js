8471bec13f012fbe3137ee36236a52d40d7e4c04
Harold <haroldbustamante22@gmail.com>
2022-12-10 15:18:44 -0400

    enlace de axios

--- client/src/api/news.api.js
+++ client/src/api/news.api.js
@@ -1,16 +1,34 @@
 import axios from "axios";
 
+// export const getNewsRequest = async () =>
+//   await axios.get("http://localhost:4000/news");
+
+// export const createNewRequest = async (niw) =>
+//   await axios.post("http://localhost:4000/news", niw);
+
+// export const deleteNewRequiest = async (id) =>
+//   await axios.delete(`http://localhost:4000/news/${id}`);
+
+//   export const getNewRequest = async (id) =>
+//   await axios.get(`http://localhost:4000/news/${id}`);
+
+//   export const updateNewRequest = async (id, newFields) =>
+//   await axios.put(`http://localhost:4000/news/${id}`, newFields);
+
 export const getNewsRequest = async () =>
-  await axios.get("http://localhost:4000/news");
+  await axios.get("https://smsc-upea-production.up.railway.app/news");
 
 export const createNewRequest = async (niw) =>
-  await axios.post("http://localhost:4000/news", niw);
+  await axios.post("https://smsc-upea-production.up.railway.app/news", niw);
 
 export const deleteNewRequiest = async (id) =>
-  await axios.delete(`http://localhost:4000/news/${id}`);
+  await axios.delete(`https://smsc-upea-production.up.railway.app/news/${id}`);
 
-  export const getNewRequest = async (id) =>
-  await axios.get(`http://localhost:4000/news/${id}`);
+export const getNewRequest = async (id) =>
+  await axios.get(`hhttps://smsc-upea-production.up.railway.app/news/${id}`);
 
-  export const updateNewRequest = async (id, newFields) =>
-  await axios.put(`http://localhost:4000/news/${id}`, newFields);
+export const updateNewRequest = async (id, newFields) =>
+  await axios.put(
+    `https://smsc-upea-production.up.railway.app/news/${id}`,
+    newFields
+  );
--- client/src/api/users.api.js
+++ client/src/api/users.api.js
@@ -1,7 +1,13 @@
 import axios from "axios";
 
+// export const getUsersRequest = async () =>
+//   await axios.get("http://localhost:4000/users");
+
+// export const getUserRequest = async (id) =>
+//   await axios.get(`http://localhost:4000/users/${id}`);
+
 export const getUsersRequest = async () =>
-  await axios.get("http://localhost:4000/users");
+  await axios.get("https://smsc-upea-production.up.railway.app/users");
 
 export const getUserRequest = async (id) =>
-  await axios.get(`http://localhost:4000/users/${id}`);
+  await axios.get(`https://smsc-upea-production.up.railway.app/users/${id}`);
