b6664529fea907603181f197809e72dfc0c0fcd7
Harold <haroldbustamante22@gmail.com>
2022-12-19 11:19:36 -0400

    Modificación del Axios

--- client/src/api/users.api.js
+++ client/src/api/users.api.js
@@ -1,16 +1,16 @@
 import axios from "axios";
 
-export const getUsersRequest = async () =>
-  await axios.get("http://localhost:4000/users");
-
-export const getUserRequest = async (id) =>
-  await axios.get(`http://localhost:4000/users/${id}`);
-
-export const authenticateUserRequest = async (userFields) =>
-  await axios.get(`http://localhost:4000/authenticate}`, userFields);
-
 // export const getUsersRequest = async () =>
-//   await axios.get("https://smsc-upea-production.up.railway.app/users");
+//   await axios.get("http://localhost:4000/users");
 
 // export const getUserRequest = async (id) =>
-//   await axios.get(`https://smsc-upea-production.up.railway.app/users/${id}`);
+//   await axios.get(`http://localhost:4000/users/${id}`);
+
+// export const authenticateUserRequest = async (userFields) =>
+//   await axios.get(`http://localhost:4000/authenticate}`, userFields);
+
+export const getUsersRequest = async () =>
+  await axios.get("https://smsc-upea-production.up.railway.app/users");
+
+export const getUserRequest = async (id) =>
+  await axios.get(`https://smsc-upea-production.up.railway.app/users/${id}`);
